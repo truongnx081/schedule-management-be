934bb864fab8fc8cdfd7e4c8ead03b861b976649
hiennt2107 <nguyentrunghieu4uio@gmail.com>
2024-12-09 12:11:59 +0700

    Viet them api cho combobox

--- src/main/java/com/fpoly/backend/controller/SemesterController.java
+++ src/main/java/com/fpoly/backend/controller/SemesterController.java
@@ -1,11 +1,37 @@
 package com.fpoly.backend.controller;
 
+import com.fpoly.backend.dto.BlockDTO;
+import com.fpoly.backend.dto.Response;
+import com.fpoly.backend.dto.SemesterDTO;
+import com.fpoly.backend.exception.AppUnCheckedException;
+import com.fpoly.backend.services.BlockService;
+import com.fpoly.backend.services.SemesterService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.validation.annotation.Validated;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+
+import java.time.LocalDateTime;
+import java.util.List;
 
 @RestController
 @RequestMapping("/api/semesters")
 @Validated
 public class SemesterController {
+    @Autowired
+    SemesterService semesterService;
+
+    @GetMapping
+    public ResponseEntity<Response> getAllSemester(){
+        try{
+            List<SemesterDTO> semesterDTOS = semesterService.getAllSemester();
+            return ResponseEntity.ok(new Response(LocalDateTime.now(),semesterDTOS, "Lấy toàn bộ semester thành công", HttpStatus.OK.value()));
+        }catch (AppUnCheckedException e){
+            return ResponseEntity.status(e.getStatus()).body(new Response(LocalDateTime.now(),null,e.getMessage(),e.getStatus().value()));
+        }
+
+    }
 }
--- src/main/java/com/fpoly/backend/services/MajorService.java
+++ src/main/java/com/fpoly/backend/services/MajorService.java
@@ -1,9 +1,14 @@
 package com.fpoly.backend.services;
 
+import com.fpoly.backend.dto.MajorDTO;
 import com.fpoly.backend.entities.Major;
 import org.springframework.stereotype.Service;
+
+import java.util.List;
 
 @Service
 public interface MajorService {
     Major findById(Integer id);
+
+    List<MajorDTO> getAllMajor();
 }
